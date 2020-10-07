86bd139163292ad594e7679eba17729c6150dcfa
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-07 12:21:28 -0400

    reader model and controller for Reader and model and controller for Book

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Bookshelf</h1>
-        <p className="lead">
-          What you're reading want to read and have already read - sorted.
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/recipes"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          View Bookshelf
-        </Link>
-      </div>
+    <div>
+        <div className="hero">
+            <h1 className="title">Bookshelf</h1>
+            <p className="description">
+                A simple way to organize your 📚
+            </p>
+            <Link
+                to="/recipes"
+                className="button"
+                role="button"
+            >
+                View Bookshelf
+            </Link>
+        </div>
     </div>
-  </div>
 );
