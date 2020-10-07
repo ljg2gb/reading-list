4a9cd8b90a5a0b53e75265f3d700fc32890b0e58
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-07 12:48:38 -0400

    adds full CRUD actions to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+    def index
+        books = Book.all
+        render json: books
+    end
+
+    def show
+        book = Book.find(params[:id])
+        render json: book
+    end
+
+    def create
+        book = Book.create!(book_params)
+        render json: book
+    end
+
+    def update
+        book = Book.find(params[:id])
+        book.update(
+            title: params[:title]
+            author: params[:author]
+            status: params[:status]
+        )
+        render json: book
+    end
+
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        render json: {message: "Book successfully deleted"}
+    end
+
+    private
+    def book_params
+        params.permit(:title, :author, :status)
+    end 
 end
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../components/Home";
 
+
 export default (
     <Router>
         <Switch>
             <Route path="/" exact component={Home} />
+
         </Switch>
     </Router>
 )
