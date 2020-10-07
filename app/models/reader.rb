f390a2034e9237fa7e7d8561c1de86247f53d30c
Lydia Gregory <ljg2gb@virginia.edu>
2020-10-07 12:22:48 -0400

    removes reader resources

--- app/models/reader.rb
+++
@@ -1,2 +0,0 @@
-class Reader < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :books
-  resources :readers
   root 'home#index'
   get '/*path' => 'home#index'
 end
